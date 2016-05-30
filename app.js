dfe108920f063285ed85e3d9c98fdded56fc568d
moneydice <paybit2015@gmail.com>
2016-05-30 16:25:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var config = {
   app_name: 'moneydice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LdThCATAAAAANmnseWXcIaTCK90h6PllpY6ilKp',  // <----- EDIT ME!
+  recaptcha_sitekey: '6LchPCETAAAAAHey8-K7C9rBRwiYpAbIJrzTN9Jv',  // <----- EDIT ME!
   redirect_uri: 'https://moneydice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
